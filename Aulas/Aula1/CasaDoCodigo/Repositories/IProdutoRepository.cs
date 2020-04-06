af2e20f61516118150b384605c10011d7863608c
Joao Moreira <jonnes04@hotmail.com>
2020-04-05 21:33:29 -0300

    Adicionados metodos para controle da session

---
+++ Aulas/Aula1/CasaDoCodigo/Repositories/IProdutoRepository.cs
@@ -0,0 +1,17 @@
+﻿using CasaDoCodigo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Repositories
+{
+    public interface IProdutoRepository
+    {
+
+        void SaveProdutos(List<Livro> livros);
+
+        IList<Produto> GetProdutos();
+
+    }
+}
